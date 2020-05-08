e8257982a81b293a5df69fe034a183c6c71f6422
web-developer77 <koa06800@gmail.com>
2020-05-08 09:50:30 -0600

    Try @rollup/plugin-typescript (work in progress)

--- scripts/builds/index.js
+++ scripts/builds/index.js
@@ -2,8 +2,10 @@ import ReactRouterBuilds from './react-router.js';
 import ReactRouterDOMBuilds from './react-router-dom.js';
 import ReactRouterNativeBuilds from './react-router-native.js';
 
-export default [
-  ...ReactRouterBuilds,
-  ...ReactRouterDOMBuilds,
-  ...ReactRouterNativeBuilds
-];
+export default ReactRouterBuilds;
+
+// export default [
+//   ...ReactRouterBuilds,
+//   ...ReactRouterDOMBuilds,
+//   ...ReactRouterNativeBuilds
+// ];
