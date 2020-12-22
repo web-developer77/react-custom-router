1582dec42021f66566038b4f12983e9cf47c03bc
web-developer77 <koa06800@gmail.com>
2020-12-21 22:41:10 -0700

    couple upgrades

--- packages/react-router/jest-transformer.js
+++ packages/react-router/jest-transformer.js
@@ -1,6 +1,9 @@
-const babelJest = require("babel-jest");
+const babelJest = require('babel-jest');
 
 module.exports = babelJest.createTransformer({
-  presets: [["@babel/preset-env", { loose: true }], "@babel/preset-react"],
-  plugins: ["babel-plugin-dev-expression"]
+  presets: [
+    ['@babel/preset-env', { loose: true, targets: { node: 'current' } }],
+    '@babel/preset-react'
+  ],
+  plugins: ['babel-plugin-dev-expression']
 });
